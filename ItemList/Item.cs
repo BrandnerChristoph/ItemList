fead05a8c8ed01a976677f9635856c6025708813
Brandner Christoph <bn@htlwy.at>
2022-09-15 20:53:59 +0200

    create Item class

--- ItemList/Item.cs
+++ ItemList/Item.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Automation;
 
 namespace ItemList
 {
 
     public enum Unit
     {
-
+        Box,
+        Piece,
+        Palette,
     }
     public class Item
     {
@@ -18,7 +21,25 @@ namespace ItemList
         public string Details { get; set; }
         public double Amount { get; set; }
         public int Quantity { get; set; }
-        public string Unit { get; set; }
+        public Unit Unit { get; set; }
 
+        public Item(){
+
+        }
+
+        public Item(string name = "", string details = "", double amount = 0, int quantity = 0, Unit unit = Unit.Piece)
+        {
+            Id = 
+            Name = name;
+            Details = details;
+            Amount = amount;
+            Quantity = quantity;
+            Unit = unit;
+        }
+
+        public override string ToString()
+        {
+            return (this.Name + " " + this.Quantity.ToString() + " " + this.Unit.ToString()).Trim() ;
+        }
     }
 }
