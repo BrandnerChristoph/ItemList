a3b21b1b67535f141939c43f3f9e0f8c65a2335b
Brandner Christoph <bn@htlwy.at>
2022-09-15 18:42:06 +0200

    Projektdateien hinzufügen.

---
+++ ItemList/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemList
+{
+
+    public enum Unit
+    {
+
+    }
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public double Amount { get; set; }
+        public int Quantity { get; set; }
+        public string Unit { get; set; }
+
+    }
+}
