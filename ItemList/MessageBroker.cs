b5688047f7dd46d3e1a9a5ea8f2797054e186545
Brandner Christoph <bn@htlwy.at>
2022-09-15 21:25:33 +0200

    added MessageBroker

---
+++ ItemList/MessageBroker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemList
+{
+    public class MessageBroker
+    {
+        public string Message { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
